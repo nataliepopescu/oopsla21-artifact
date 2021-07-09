16c1c88dd067e5068e133e3049f204e57e08ab70
Ziyang Xu <ziyangx@princeton.edu>
2021-07-09 00:08:19 -0400

    update hotness script to handle O0

--- genHotness.sh
+++ genHotness.sh
@@ -4,6 +4,15 @@
 script_path=`realpath $0`
 root_path=`dirname $script_path`
 
-valgrind --tool=callgrind --callgrind-out-file=callgrind.out $1 $2
+# already in explore-source/exp-xx/
+ln -sf ../../Cargo.toml .
+ln -sf ../../bin .
+
+rm -rf target
+RUSTFLAGS="-g" cargo build --jobs 1 --bin $1 > log 2>&1
+cp target/debug/$1 exp.exe
+#rm -rf target
+
+valgrind --tool=callgrind --callgrind-out-file=callgrind.out ./exp.exe $2
 perl $root_path/callgrind_get_unchecked_parser.perl --auto=yes callgrind.out > cal.out
 
