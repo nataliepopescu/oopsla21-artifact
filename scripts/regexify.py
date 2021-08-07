a3d58d6ea588444cc0a91f1b87ba145003d79586
Natalie Popescu <nataliepopescu@gmail.com>
2021-08-07 19:43:33 -0400

    semi working regexify

--- scripts/regexify.py
+++ scripts/regexify.py
@@ -4,6 +4,7 @@ import re
 import os
 import subprocess
 import argparse
+from make_patch import patchAll
 
 
 # [(line, col, fname), ...]
@@ -21,60 +22,85 @@ def dumpBCs(bcs, logfile=None):
             fd.write("%d %d %s\n" % (line, col, fname))
 
 
-def argParse():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--root", "-r",
-            metavar="path",
-            type=str,
-            help="root path from where to start source code conversions")
-    parser.add_argument("--single-file", "-s",
-            metavar="filename",
-            type=str,
-            nargs="?",
-            help="Specify this if only want to apply this tool to a single file")
-    parser.add_argument("--logfile", "-l",
-            metavar="filename",
-            type=str,
-            nargs="?",
-            const="changes.txt",
-            default="changes.txt",
-            help="name of the file in which to store line/column/filename of changes; "\
-                    "default is 'changes.txt' in the specified root dir")
-    args = parser.parse_args()
-    return args.root, args.logfile, args.single_file
+def findTargetFiles(mir_filelist): #single_file):
+    # a line in mir_filelist looks something like: src/lib.rs:28:42: 28:62 (#0)
+    filelist = dict()
 
+    lines = mir_filelist.strip().split("\n")
+    for line in lines: 
+        parts = line.split(":")
+        print(parts)
+        filename = parts[0].strip()
+        line_start = parts[1].strip()
+        line_end = parts[3].strip()
+        col_start = parts[2].strip()
+        col_end = parts[4].split("(")[0].strip()
 
-def findTargetFiles(single_file):
-    # files
-    if single_file:
-        target = single_file
-    else:
-        target = "."
-    rs_files = subprocess.run(["find", target, "-name", "*.rs", "-type", "f"], 
-            capture_output=True, text=True)
-    filelist = rs_files.stdout.split()
-    if os.path.exists("ignore.txt"):
-        with open("ignore.txt", 'r') as fd:
-            lines = fd.readlines()
-            ignore_list = [i.strip() for i in lines]
+        # dictionary structure: 
+        #  { 
+        #    filename1: [
+        #        [line_start, line_end, col_start, col_end], 
+        #    ],
+        #    filename2: [
+        #        [line_start, line_end, col_start, col_end], 
+        #        [line_start, line_end, col_start, col_end], 
+        #    ],
+        #  }
+        if filelist.get(filename): 
+            #print("filename already exists")
+            entries = filelist.get(filename)
+            new_entry = int(line_start) #, 
+            #    int(line_end), 
+            #    int(col_start), 
+            #    int(col_end)
+            #]
+            if new_entry in entries: 
+                continue
+            entries.append(new_entry)
+            filelist.update({filename: entries})
+        else: 
+            #print("filename does not yet exist")
+            entry = int(line_start) #, 
+            #    int(line_end), 
+            #    int(col_start), 
+            #    int(col_end)
+            #]
+            filelist.update({filename: [entry]})
 
-        final_list = filelist.copy()
-        for f in filelist:
-            for ignore in ignore_list:
-                if ignore in f:
-                    if f in final_list:
-                        final_list.remove(f)
-
-        print("Removed", len(filelist) - len (final_list), "files")
-        filelist = final_list
     return filelist
+
+
+#def findTargetFiles(mir_filelist): #single_file):
+    # files
+    #if single_file:
+    #    target = single_file
+    #else:
+    #    target = "."
+    #rs_files = subprocess.run(["find", target, "-name", "*.rs", "-type", "f"], 
+    #        capture_output=True, text=True)
+    #filelist = rs_files.stdout.split()
+    #if os.path.exists("ignore.txt"):
+    #    with open("ignore.txt", 'r') as fd:
+    #        lines = fd.readlines()
+    #        ignore_list = [i.strip() for i in lines]
+
+    #    final_list = filelist.copy()
+    #    for f in filelist:
+    #        for ignore in ignore_list:
+    #            if ignore in f:
+    #                if f in final_list:
+    #                    final_list.remove(f)
+
+    #    print("Removed", len(filelist) - len (final_list), "files")
+    #    filelist = final_list
+    #return filelist
 
 
 # Convert all things in place in one file
 # old_fname is the file before conversion
 # new_fname is the file after conversion 
 # selective unsafe contains the lines that we want to keep the unsafe
-def convertFile(old_fname, new_fname, selective_unsafe=[]):
+def convertFile(old_fname, new_fname, selective_safe=[]):
     # mutregex_in = r'([($a-zA-Z_][a-zA-Z0-9:_\.\(\)\*]*)\.get_unchecked_mut\(' #]([0-9a-zA-Z_][a-zA-Z0-9_\.\>\*\+ ]*)[)]'
     # mutregex_out = r'(&mut \1[' #\2])'
     # regex_in = r'([($a-zA-Z_][a-zA-Z0-9:_\.\(\)\*]*)\.get_unchecked\(' #]([0-9a-zA-Z_][a-zA-Z0-9_\.\>\*\+ ]*)[)]'
@@ -89,37 +115,36 @@ def convertFile(old_fname, new_fname, selective_unsafe=[]):
     mutregex_out = r'.get_mut(' #\2])'
     regex_in = r'\.get_unchecked\(' #]([0-9a-zA-Z_][a-zA-Z0-9_\.\>\*\+ ]*)[)]'
     regex_out = r'.get(' #\2])'
-    raw_mutregex_in = r'\.get_unchecked_raw_mut\(' #]([0-9a-zA-Z_][a-zA-Z0-9_\.\>\*\+ ]*)[)]'
-    raw_mutregex_out = r'.get_mut(' #\2])'
-    raw_regex_in = r'\.get_unchecked_raw\(' #]([0-9a-zA-Z_][a-zA-Z0-9_\.\>\*\+ ]*)[)]'
-    raw_regex_out = r'.get(' #\2])'
+    #raw_mutregex_in = r'\.get_unchecked_raw_mut\(' #]([0-9a-zA-Z_][a-zA-Z0-9_\.\>\*\+ ]*)[)]'
+    #raw_mutregex_out = r'.get_mut(' #\2])'
+    #raw_regex_in = r'\.get_unchecked_raw\(' #]([0-9a-zA-Z_][a-zA-Z0-9_\.\>\*\+ ]*)[)]'
+    #raw_regex_out = r'.get(' #\2])'
     
-
     with open(old_fname, 'r') as fd:
         old_block = fd.read()
 
-    block, bcs_mut = convertBlock(old_block, mutregex_in, mutregex_out, 1, selective_unsafe)
+    block, bcs_mut = convertBlock(old_block, mutregex_in, mutregex_out, 1, selective_safe)
     if block == None or bcs_mut == None:
         print("Conversion failed in file %s" % old_fname)
         exit()
 
-    block, bcs_immut = convertBlock(block, regex_in, regex_out, 1, selective_unsafe)
+    block, bcs_immut = convertBlock(block, regex_in, regex_out, 1, selective_safe)
     if block == None or bcs_immut == None:
         print("Conversion failed in file %s" % old_fname)
         exit()
 
-    block, bcs_raw_mut = convertBlock(block, raw_mutregex_in, raw_mutregex_out)
-    if block == None or bcs_raw_mut == None:
-        print("Conversion failed in file %s" % old_fname)
-        exit()
+    #block, bcs_raw_mut = convertBlock(block, raw_mutregex_in, raw_mutregex_out)
+    #if block == None or bcs_raw_mut == None:
+    #    print("Conversion failed in file %s" % old_fname)
+    #    exit()
 
-    block, bcs_raw_immut = convertBlock(block, raw_regex_in, raw_regex_out)
-    if block == None or bcs_raw_immut == None:
-        print("Conversion failed in file %s" % old_fname)
-        exit()
+    #block, bcs_raw_immut = convertBlock(block, raw_regex_in, raw_regex_out)
+    #if block == None or bcs_raw_immut == None:
+    #    print("Conversion failed in file %s" % old_fname)
+    #    exit()
 
     new_block = block
-    bcs = bcs_mut + bcs_immut + bcs_raw_mut + bcs_raw_immut 
+    bcs = bcs_mut + bcs_immut # + bcs_raw_mut + bcs_raw_immut 
 
     with open(new_fname, 'w') as fd:
         fd.write(new_block)
@@ -132,6 +157,7 @@ def convertFile(old_fname, new_fname, selective_unsafe=[]):
 
 # already have a left parenthesis, find the matching right parenthesis
 def findMatchingParenthsis(block):
+    # FIXME MIR pass returns full span
     depth = 1
 
     for pos, ch in enumerate(block):
@@ -145,13 +171,18 @@ def findMatchingParenthsis(block):
     # failed case 
     return -1
 
+
 # when a x.get_unchecked( or x.get_unchecked_mut( is found
 # Step 1: go back and replace the handle with regex
 # Step 2: go forward and find matching parenthesis, replace it as "])"
 # Step 3: convert whatever is inside
-def convertBlock(block, regex_in, regex_out, cur_line=1, selective_unsafe=[]):
+def convertBlock(block, regex_in, regex_out, cur_line=1, selective_safe=[]):
     bcs = []
     new_block = ""
+
+    # block == file
+    # need finer re matching granularity, because some functions may not be 
+    # converted in this file
 
     # convert all instances of regex_in (per line) one at a time
     while (match := re.search(regex_in, block)): 
@@ -162,7 +193,6 @@ def convertBlock(block, regex_in, regex_out, cur_line=1, selective_unsafe=[]):
 
         # handle within function call
         # foo(xx.get_uncheck(
-
         right = 0
         for idx in reversed(range(len(cur_block) - 1)): # exclude the last '('
             if cur_block[idx] == ')':
@@ -187,7 +217,9 @@ def convertBlock(block, regex_in, regex_out, cur_line=1, selective_unsafe=[]):
             old_col = len(pre_block) - old_col
 
         # if choose not to convert this line to safe
-        if cur_line in selective_unsafe:
+        if not cur_line in selective_safe:
+            print("we here")
+            print(cur_line)
             new_block += pre_block + cur_block
             block = post_block
             continue
@@ -208,7 +240,7 @@ def convertBlock(block, regex_in, regex_out, cur_line=1, selective_unsafe=[]):
         cur_block = re.sub(regex_in, regex_out, cur_block, count=1)
         new_col = old_col + len(cur_block) - 1
 
-        new_middle_block, addition_bcs = convertBlock(post_block[:pos], regex_in, regex_out, cur_line, selective_unsafe)
+        new_middle_block, addition_bcs = convertBlock(post_block[:pos], regex_in, regex_out, cur_line, selective_safe)
 
         # add bounds checks from the middle block to the bcs
         if addition_bcs is None:
@@ -236,18 +268,66 @@ def convertBlock(block, regex_in, regex_out, cur_line=1, selective_unsafe=[]):
     return new_block, bcs
 
 
+def argParse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--root", "-r",
+            metavar="path",
+            type=str,
+            help="root path from where to start source code conversions")
+    #parser.add_argument("--single-file", "-s",
+    #        metavar="filename",
+    #        type=str,
+    #        nargs="?",
+    #        help="Specify this if only want to apply this tool to a single file")
+    parser.add_argument("--mir-filelist", "-f",
+            metavar="filename",
+            required=False,
+            type=str,
+            nargs=1,
+            default="mir_output.txt",
+            help="Specify the mir output file that contains the filenames, line #s, and col #s /"
+            "of the function calls to convert") #this if only want to apply this tool to a single file")
+    parser.add_argument("--logfile", "-l",
+            metavar="filename",
+            type=str,
+            nargs="?",
+            const="changes.txt",
+            default="changes.txt",
+            help="name of the file in which to store line/column/filename of changes; "\
+                    "default is 'changes.txt' in the specified root dir")
+    args = parser.parse_args()
+    return args.root, args.logfile, args.mir_filelist
+
+
 if __name__ == "__main__":
-    root, logfile, single_file = argParse()
+    root, logfile, mir_filelist = argParse()
     os.chdir(root)
 
-    filelist = findTargetFiles(single_file)
-    
-    print("Converting files: ", filelist)
+    # vendor
+    subprocess.run(["cargo", "clean"])
+    subprocess.run(["cargo", "vendor", "--versioned-dirs"])
+    subprocess.run(["mkdir", "-p", ".cargo"])
+    with open(".cargo/config.toml", 'a') as fd: 
+        fd.write("[source.crates-io]\nreplace-with = \x22vendored-sources\x22\n[source.vendored-sources]\ndirectory = \x22vendor\x22")
 
+    # patch Cargo.toml with vendored crates
+    patchAll(".", "vendor", "vendor")
+
+    # get list of get_unchecked to convert
+    os.environ["RUSTFLAGS"] = "-Z convert-unchecked-indexing"
+    mir_filelist = subprocess.run(["cargo", "bench", "--no-run"], capture_output=True, text=True)
+
+    filelist = findTargetFiles(mir_filelist.stdout)
+    print("\nFilelist: \n")
+    for f in filelist.items():
+        print(f)
+    print()
+    
     # List of all bounds checks
     bcs = []
-    for fname in filelist:
-        bcs.extend(convertFile(fname, fname))
+    for fname in filelist.keys():
+        print("Converting: {}".format(fname))
+        bcs.extend(convertFile(fname, fname, filelist[fname]))
 
     dumpBCs(bcs, logfile)
 
